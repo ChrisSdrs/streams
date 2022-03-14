871969d3d128ee7021e6ace10cdc18b144fb374a
Christos <xristos94sideris@gmail.com>
2022-03-14 21:56:23 +0200

    Initial commit

---
+++ client/src/components/GoogleAuth.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class GoogleAuth extends React.Component {
+  componentDidMount() {
+    window.gapi.load("client:auth2");
+  }
+
+  render() {
+    return <div>Google Auth</div>;
+  }
+}
+
+export default GoogleAuth;
